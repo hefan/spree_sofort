446d8d3b4b02780ec7f9b039c50fe4bd6a6e29a5
Stefan Hartmann <stefan@pfeffans.local>
2014-06-20 19:20:13 +0200

    formatting

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Spree::Core::Engine.routes.draw do
-
   get '/payment_network/success', :to => 'payment_network#success'
   get '/payment_network/cancel', :to => 'payment_network#cancel'
   post '/payment_network/status', :to => 'payment_network#status'
-
 end
