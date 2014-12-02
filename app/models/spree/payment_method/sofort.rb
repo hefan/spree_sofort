fd02e128fb30d9b365b80b32a3eaf56e237f045a
Stefan Hartmann <stefan@pfeffans.local>
2014-12-02 20:27:36 +0100

    possibility to cancel payment in admin backend ignoring empty transactions status change requests do not verify_authenticity_token when status is called via post set sofort payment complete

--- app/models/spree/payment_method/sofort.rb
+++ app/models/spree/payment_method/sofort.rb
@@ -1,6 +1,6 @@
 # encoding: UTF-8
 module Spree
-  class PaymentMethod::Sofort < PaymentMethod
+  class PaymentMethod::Sofort < PaymentMethod::Check
     preference :config_key, :string  # config key is USER_ID:PROJECT_ID:API_KEY
     preference :server_url, :string, :default => "https://api.sofort.com/api/xml"
   end
