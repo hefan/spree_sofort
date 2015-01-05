40b728bc17ac5717cbcabbedb06f210f9fc7b121
Stefan Hartmann <stefan@wemove.com>
2015-01-06 00:32:24 +0100

    added reference prefix and suffix

--- app/models/spree/payment_method/sofort.rb
+++ app/models/spree/payment_method/sofort.rb
@@ -3,5 +3,7 @@ module Spree
   class PaymentMethod::Sofort < PaymentMethod::Check
     preference :config_key, :string  # config key is USER_ID:PROJECT_ID:API_KEY
     preference :server_url, :string, :default => "https://api.sofort.com/api/xml"
+    preference :reference_prefix, :string, :default => ""
+    preference :reference_suffix, :string, :default => ""    
   end
 end
