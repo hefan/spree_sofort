6b9db1fcf59ca4d722cce57315bca20dae0b0343
Stefan Hartmann <stefan@pfeffans.local>
2015-10-08 15:05:17 +0200

    working payment transitions

--- app/services/spree/sofort_service.rb
+++ app/services/spree/sofort_service.rb
@@ -51,9 +51,9 @@ module Spree
     def alter_payment_status transaction_details
       if transaction_details["status"].present?
         if transaction_details["status"].eql? "loss"
-          @sofort_payment.pend!
+          @sofort_payment.process! # could be received after a loss -> no failure or void here
         elsif transaction_details["status"].eql? "pending"
-          @sofort_payment.pend!
+          @sofort_payment.process!
         elsif transaction_details["status"].eql? "refunded"
           @sofort_payment.void!
         else # received
