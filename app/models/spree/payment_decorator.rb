ec85a6b54373ee246132cb35702170aad40adc25
Stefan Hartmann <stefan@wemove.com>
2016-08-21 03:00:11 +0200

    remove unused scopes

--- app/models/spree/payment_decorator.rb
+++
@@ -1,3 +0,0 @@
-Spree::Payment.class_eval do 
-  scope :sofort, -> { where source_type: "Spree::Sofort"}
-end
--- app/models/spree/payment_method_decorator.rb
+++
@@ -1,3 +0,0 @@
-Spree::PaymentMethod.class_eval do 
-  scope :sofort, -> { where type: "Spree::PaymentMethod::Sofort"}
-end
