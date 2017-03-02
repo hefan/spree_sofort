7f7819a2054786fc880f73e839cbe81fcb10971c
Stefan Hartmann <stefan@pfeffans.local>
2017-03-02 19:24:59 +0100

    first changes for spree 3.2

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,15 +2,7 @@ require 'webmock/rspec'
 
 # Run Coverage report
 require 'simplecov'
-SimpleCov.start do
-  add_filter 'spec/dummy'
-  add_group 'Controllers', 'app/controllers'
-  add_group 'Helpers', 'app/helpers'
-  add_group 'Mailers', 'app/mailers'
-  add_group 'Models', 'app/models'
-  add_group 'Views', 'app/views'
-  add_group 'Libraries', 'lib'
-end
+SimpleCov.start 'rails'
 
 # Configure Rails Environment
 ENV['RAILS_ENV'] = 'test'
@@ -18,59 +10,49 @@ ENV['RAILS_ENV'] = 'test'
 require File.expand_path('../dummy/config/environment.rb',  __FILE__)
 
 require 'rspec/rails'
-require 'database_cleaner'
 require 'ffaker'
 
-# Requires supporting ruby files with custom matchers and macros, etc,
-# in spec/support/ and its subdirectories.
-Dir[File.join(File.dirname(__FILE__), 'support/**/*.rb')].each { |f| require f }
+require 'database_cleaner'
 
 # Requires factories defined in spree_core
 require 'spree/testing_support/factories'
 require 'spree/testing_support/controller_requests'
 require 'spree/testing_support/authorization_helpers'
 require 'spree/testing_support/url_helpers'
+require 'spree/testing_support/preferences'
+require 'spree/api/testing_support/helpers'
+require 'spree/api/testing_support/setup'
+require 'spree/testing_support/capybara_ext'
 
 # Requires factories defined in lib/spree_sofort/factories.rb
 require 'spree_sofort/factories'
 
+# Requires supporting ruby files with custom matchers and macros, etc,
+# in spec/support/ and its subdirectories.
+Dir[File.join(File.dirname(__FILE__), 'support/**/*.rb')].each { |f| require f }
+
 RSpec.configure do |config|
   config.expose_current_running_example_as :example
+
   config.infer_spec_type_from_file_location!
-	config.include Spree::TestingSupport::ControllerRequests, :type => :controller
-
-  config.include FactoryGirl::Syntax::Methods
-
-  # == URL Helpers
-  #
-  # Allows access to Spree's routes in specs:
-  #
-  # visit spree.admin_path
-  # current_path.should eql(spree.products_path)
-  config.include Spree::TestingSupport::UrlHelpers
-
-  # == Mock Framework
-  #
-  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-  #
-  # config.mock_with :mocha
-  # config.mock_with :flexmock
-  # config.mock_with :rr
   config.mock_with :rspec
   config.color = true
 
-  # Capybara javascript drivers require transactional fixtures set to false, and we use DatabaseCleaner
-  # to cleanup after each test instead.  Without transactional fixtures set to false the records created
-  # to setup a test will be unavailable to the browser, which runs under a separate server instance.
+  config.filter_run focus: true
+  config.run_all_when_everything_filtered = true
   config.use_transactional_fixtures = false
 
-  # Ensure Suite is set to use transactions for speed.
+  config.include FactoryGirl::Syntax::Methods
+
+  config.include Spree::TestingSupport::UrlHelpers
+
+  config.include Spree::TestingSupport::ControllerRequests, type: :controller
+  config.include Spree::Api::TestingSupport::Helpers, type: :controller
+
   config.before :suite do
-    DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.clean_with :truncation
   end
 
-  # Before each spec check if it is a Javascript test and switch between using database transactions or not where necessary.
   config.before :each do
     DatabaseCleaner.strategy = example.metadata[:js] ? :truncation : :transaction
     DatabaseCleaner.start
--- spree_sofort.gemspec
+++ spree_sofort.gemspec
@@ -2,7 +2,7 @@
 Gem::Specification.new do |s|
   s.platform    = Gem::Platform::RUBY
   s.name        = 'spree_sofort'
-  s.version     = '3.1'
+  s.version     = '3.2'
   s.summary     = 'Add sofortueberweisung payment to spree'
   s.description = ''
   s.required_ruby_version = '>= 1.9.3'
@@ -15,7 +15,7 @@ Gem::Specification.new do |s|
   s.require_path = 'lib'
   s.requirements << 'none'
 
-  s.add_dependency 'spree_core', '~> 3.0'
+  s.add_dependency 'spree_core', '> 3.1'
   s.add_dependency 'httparty'
   s.add_dependency 'actionpack-xml_parser'
 
